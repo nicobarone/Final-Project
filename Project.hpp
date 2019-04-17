951708f9199c121d2137dc282fe3937130b3c7ae
nicobarone <49491360+nicobarone@users.noreply.github.com>
2019-04-16 22:16:53 -0600

    Update Project.hpp

--- Project.hpp
+++ Project.hpp
@@ -20,3 +20,31 @@ class LL
     void insertend(int value, int suit);
 		void printListstart();
 };
+
+#ifndef HEAP_HPP
+#define HEAP_HPP
+
+class Heap
+{
+	public:
+
+	Heap(int heapSize);
+	~Heap();
+	void printHeap();
+	void addToHeap (int value);
+	int removeFromHeap();
+	int peekValue();
+	bool isFull();
+	bool isEmpty();
+
+	private:
+
+	//void repairUpward(int nodeIndex);
+	void repairDownward(int nodeIndex);
+
+	int* heap; //pointer to the array
+	int currentSize; //current number of students in heap
+	int heapSize; //maximum capacity of the heap
+};
+#endif
+
