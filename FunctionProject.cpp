e15816fc9907f9a30dc09ccb1437da72ea314a52
nicobarone <49491360+nicobarone@users.noreply.github.com>
2019-04-16 22:42:43 -0600

    Update FunctionProject.cpp

--- FunctionProject.cpp
+++ FunctionProject.cpp
@@ -41,14 +41,137 @@ void LL::printListstart(){
   cout<<temp->value<<endl;
 }
 
-/*void LL::createcards();
+int parent(int index) {return (index-1)/2;}
+int leftChild(int index) {return 2*index+1;}
+int rightChild(int index) {return 2*index+2;}
+void swap(int &x, int &y) {int z = x; x = y; y = z;}
+
+Heap::Heap(int heapSize) {
+  currentSize = 0;
+  heap = new int[heapSize];
+}
+
+Heap::~Heap(){
+  delete[] heap;
+}
+
+
+void Heap::printHeap() {
+  for(int i = 0; i < currentSize; i++) cout<< heap[i] << endl;
+}
+
+/* Max Heap */
+
+void Heap::addToHeap (int value) {
+  if (currentSize == heapSize) {
+    cout << "Heap overflow" << endl;
+  }
+  else {
+    // insert a value in a heap
+    currentSize++;
+    heap[currentSize-1] = value;
+    int i = currentSize-1;
+    while(parent(i) >= 0 && heap[parent(i)] < heap[i]){
+      swap(heap[i], heap[parent(i)]);
+      i = parent(i);
+    }
+  }
+}
+
+void Heap::repairDownward(int index) {
+  int max = index;
+  if (leftChild(index) < currentSize) {
+    if (heap[max] < heap[leftChild(index)]) max = leftChild(index);
+  }
+  if (rightChild(index) < currentSize) {
+    if (heap[max] < heap[rightChild(index)]) max = rightChild(index);
+  }
+
+  if (index == max) {
+    // Already heapified
+    return;
+  }
+  else {
+    swap(heap[max], heap[index]);
+    repairDownward(max);
+  }
+}
+
+/* Max Heap */
+
+int Heap::removeFromHeap() { // just for min heaps
+  if (currentSize <= 0) {
+    cout << " Heap is empty: returning garbage" << endl;
+    return -1;
+  }
+  else {
+    int result = heap[0];
+    heap[0] = heap[currentSize-1];
+    currentSize = currentSize - 1;
+    repairDownward(0);
+    return result;
+  }
+}
+
+int Heap::peekValue() {
+  if (currentSize <= 0) {
+    cout << " Heap is empty: returning garbage" << endl;
+    return -1;
+  }
+  else return heap[0];
+}
+
+bool Heap::isFull() {
+  if (currentSize == heapSize) {
+    cout << " Heap is Full " << endl;
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+bool Heap::isEmpty() {
+  if (currentSize <= 0) {
+    cout << " Heap is empty " << endl;
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+
+void LL::createhands(int* array1, int* array2)
 {
-  for(int i=2; i<=14; i++)
-  {//creates Initial deck
-	   for(int j=0; j < 4;j++)
-     {
-       //0 = hearts; 1 = diamonds; 2 = spades; 3 = clubs
-		   insertend(i,j);
-     }
-   }
-}*/
+
+	Heap hand1(7);
+	int newarray1[7];
+
+	for(int i =0; i<7;i++)
+	{
+		hand1.addToHeap(array1[i]);
+	}
+	hand1.printHeap();
+	cout<<endl;
+
+	for(int i = 0; i < 7;i++)
+	{
+		newarray1[i] = hand1.removeFromHeap();
+	}
+
+
+	Heap hand2(7);
+	int newarray2[7];
+
+	for(int i =0; i<7;i++)
+	{
+		hand2.addToHeap(array2[i]);
+	}
+	hand2.printHeap();
+
+	for(int i = 0; i < 7;i++)
+	{
+		newarray2[i] = hand2.removeFromHeap();
+	}
+}
