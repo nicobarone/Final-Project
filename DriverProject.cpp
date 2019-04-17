186f264c5d55c7b111b9ace2766d7b1dc2cb3e80
nicobarone <49491360+nicobarone@users.noreply.github.com>
2019-04-16 21:18:06 -0600

    Update DriverProject.cpp

--- DriverProject.cpp
+++ DriverProject.cpp
@@ -5,6 +5,16 @@ using namespace std;
 
 int main()
 {
-  loadcards();
+  LL ll;
+
+	for(int i=2; i<=14; i++)
+  {//creates Initial deck
+	   for(int j=0; j < 4;j++)
+     {
+       //0 = hearts; 1 = diamonds; 2 = spades; 3 = clubs
+		   ll.insertend(i,j);
+     }
+   }
+	 ll.printListstart();
   
 }
