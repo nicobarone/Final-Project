9247155c625d7bc7465f781510bf2adf4b5aeb29
nicobarone <49491360+nicobarone@users.noreply.github.com>
2019-04-21 11:47:57 -0600

    Update logicFunctions.cpp

--- logicFunctions.cpp
+++ logicFunctions.cpp
@@ -50,92 +50,32 @@ void LL::whowins(Node *player1[], Node *player2[])
   int playerval1 = 0;
   int playerval2 = 0;
 
-  if(pair(player1) == true)
-  {
-    playerval1 = 1;
-  }
-  if(pair(player2) == true)
-  {
-    playerval2 = 1;
-  }
-  if(twopair(player1) == true)
-  {
-    playerval1 = 2;
-  }
-  if(twopair(player2) == true)
-  {
-    playerval2 = 2;
-  }
-  if(threeofakind(player1) == true)
-  {
-    playerval1 = 3;
-  }
-  if(threeofakind(player2) == true)
-  {
-    playerval2 = 3;
-  }
-  if(straight(player1) == true)
-  {
-    playerval1 = 4;
-  }
-  if(straight(player2) == true)
-  {
-    playerval2 = 4;
-  }
-  if(flush(player1) == true)
-  {
-    playerval1 = 5;
-  }
-  if(flush(player2) == true)
-  {
-    playerval2 = 5;
-  }
-  if(fullhouse(player1) == true)
-  {
-    playerval1 = 6;
-  }
-  if(fullhouse(player2) == true)
-  {
-    playerval2 = 6;
-  }
-  if(fourofakind(player1) == true)
-  {
-    playerval1 = 7;
-  }
-  if(fourofakind(player2) == true)
-  {
-    playerval2 = 7;
-  }
-  if(straightflush(player1) == true)
-  {
-    playerval1 = 8;
-  }
-  if(straightflush(player2) == true)
-  {
-    playerval2 = 8;
-  }
+  
 
-  if(playerval1 == playerval2)
-  {
-    int highcard = checkhighcard(player1,player2);
-    if(highcard == 1)
-    {
-      //return 10;
-    }
-    else
-    {
-      //return 20
-    }
-  }
-  else
-  {
-    if(playerval1 > playerval2)
-    {
-      //return 10;
-    }
-    else
-    {
-      //return 20;
-    }
-  }
+  pair(player1);
+  pair(player2);
+
+  twopair(player1);
+  twopair(player2);
+
+  threeofakind(player1);
+  threeofakind(player2);
+
+  straight(player1);
+  straight(player2);
+
+  flush(player1);
+  flush(player2);
+
+  fullhouse(player1);
+  fullhouse(player2);
+
+  fourofakind(player1);
+  fourofakind(player2);
+
+  straightflush(player1);
+  straightflush(player2);
+
+
 }
+
