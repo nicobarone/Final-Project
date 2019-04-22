3624724c196aa3f95f3fff7ce37be61fe985c281
PeterGeis <49704796+PeterGeis@users.noreply.github.com>
2019-04-21 20:50:25 -0700

    Update logicFunctions.cpp

--- logicFunctions.cpp
+++ logicFunctions.cpp
@@ -246,14 +246,94 @@ void LL::fourofakind(Node *player[])
 }
 void LL::straightflush(Node *player[])
 {
-  for(int i=0;i<3;i++){
-    if((player[i]->value)+1 == player[i+1]->value && (player[i]->value)+2 == player[i+2]->value && (player[i]->value)+3 == player[i+3]->value && (player[i]->value)+4 == player[i+4]->value){
-      if(player[i]->suit == player[i+1]->suit && player[i]->suit == player[i+2]->suit && player[i]->suit == player[i+3]->suit && player[i]->suit == player[i+4]->suit){
-        player[7]->value = 9;
+  int count0=0,count1=0,count2=0,count3=0;
+  int w = 4;
+  for(int q=0;q<7;q++){
+    if(player[q]->suit == 0){
+      count0++;
+      if(count0 == 5){
+        w=0;
+      }
+    }
+    else if(player[q]->suit == 1){
+      count1++;
+      if(count1 == 5){
+        w=1;
+      }
+    }
+    else if(player[q]->suit == 2){
+      count2++;
+      if(count2 == 5){
+        w=2;
+      }
+    }
+    else if(player[q]->suit == 3){
+      count3++;
+      if(count3 == 5){
+        w=3;
       }
     }
   }
-  //cout<<player[7]->value<<endl;
+  if(w != 4){
+    int k=0;
+    int m=0;
+    int x = player[7]->value;
+    int t = player[7]->suit;
+    int y = player[8]->value;
+    player[8]->value = 100;
+    for(int i=0; i<3; i++){
+      int j=0;
+      m = i;
+    while(player[i+1]->value == (player[i]->value-1) || player[i+1]->value == player[i]->value){
+      if(player[i+1]->value != player[i]->value && player[i]->suit == w){
+        j++;
+      }
+      i++;
+      if(j == 4){
+        player[7]->value = 8;
+        k=1;
+        break;
+      }
+    }
+    if(k == 1){
+      player[7]->suit = player[m]->value;
+      break;
+    }
+    i=m;
+  }
+  if((player[7]->value != 8) && (player[0]->value == 14)){
+    player[7]->value = 1;
+    int z = 0,g=0;
+    for(int p=0;p<4;p++){
+      int n = 0;
+      z=p;
+      while(player[p+1]->value == (player[p]->value-1) || player[p+1]->value == player[p]->value){
+        if((player[p+1]->value != player[p]->value) && player[p]->suit == w){
+            n++;
+        }
+        p++;
+        if(n == 4){
+          player[7]->value = 8;
+          g=1;
+          break;
+        }
+      }
+      if(g == 1){
+        player[7]->suit = player[z]->value;
+        break;
+      }
+      p=z;
+    }
+  }
+  if(player[7]->value != 8){
+    player[7]->value = x;
+    player[7]->value = t;
+  }
+  player[8]->value = y;
+  }
+  cout<<"???????????????????????"<<endl;
+  cout<<player[7]->value<<endl;
+  cout<<player[7]->suit<<endl<<endl;
 }
 void LL::checkhighcard(Node *player[], Node *player2[])
 {
