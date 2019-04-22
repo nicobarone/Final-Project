3f58fedfcc3d1a6641427960bb37c02d287c7db4
nicobarone <49491360+nicobarone@users.noreply.github.com>
2019-04-21 21:53:26 -0600

    Update logicFunctions.cpp

--- logicFunctions.cpp
+++ logicFunctions.cpp
@@ -18,8 +18,6 @@ void LL::pair(Node *player[])
       player[7]->value = 1;
     }
   }
-  //cout<<player[7]->value<<endl;
-  //cout<<player[7]->suit<<endl;
 }
 
 void LL::twopair(Node *player[])
@@ -39,12 +37,8 @@ void LL::twopair(Node *player[])
         }
       }
     }
-  }/*
-  cout<<"------------------"<<endl;
-  cout<<player[7]->value<<endl;
-  cout<<player[7]->suit<<endl;
-  cout<<player[8]->value<<endl;
-  cout<<"------------------"<<endl;*/
+  }
+
 }
 void LL::threeofakind(Node *player[])
 {
@@ -59,10 +53,8 @@ void LL::threeofakind(Node *player[])
       player[7]->value = 3;
     }
   }
-  //cout<<player[7]->value<<endl;
-  //cout<<player[7]->suit<<endl;
-}
 
+}
 
 void LL::straight(Node *player[])
 {
@@ -109,7 +101,7 @@ void LL::straight(Node *player[])
         }
       }
       if(g == 1){
-        player[7]->suit = player[3]->value;
+        player[7]->suit = player[z]->value;
         break;
       }
       p=z;
@@ -119,10 +111,10 @@ void LL::straight(Node *player[])
     player[7]->value = x;
   }
   player[8]->value = y;
-  cout<<"???????????????????????"<<endl;
-  cout<<player[7]->value<<endl;
-  cout<<player[7]->suit<<endl<<endl;
+
 }
+
+
 
 void LL::flush(Node *player[]){
   int count0=0,count1=0,count2=0,count3=0;
@@ -180,15 +172,8 @@ void LL::flush(Node *player[]){
         }
       }
     }
-  }/*
-  cout<<"---------*---------"<<endl;
-  cout<<player[7]->value<<endl;
-  cout<<player[7]->suit<<endl;
-  cout<<player[8]->value<<endl;
-  cout<<player[8]->suit<<endl;
-  cout<<player[9]->value<<endl;
-  cout<<player[9]->suit<<endl;
-  cout<<"---------*---------"<<endl;*/
+  }
+
 }
 void LL::fullhouse(Node *player[])
 {
@@ -219,16 +204,11 @@ void LL::fullhouse(Node *player[])
             player[8]->value = player[i]->value;
           }
         }
-        //player[8]->value = 1;
+
       }
     }
   }
-  /*
-  cout<<"---------*---------"<<endl;
-  cout<<player[7]->value<<endl;
-  cout<<player[7]->suit<<endl;
-  cout<<player[8]->value<<endl;
-  cout<<"---------*---------"<<endl;*/
+
 }
 void LL::fourofakind(Node *player[])
 {
@@ -237,12 +217,8 @@ void LL::fourofakind(Node *player[])
       player[7]->value = 7;
       player[7]->suit = player[i]->value;
     }
-  }/*
-  cout<<"------------------"<<endl;
-  cout<<player[7]->value<<endl;
-  cout<<player[7]->suit<<endl;
-  //cout<<player[8]->value<<endl;
-  cout<<"------------------"<<endl;*/
+  }
+
 }
 void LL::straightflush(Node *player[])
 {
@@ -344,48 +320,6 @@ void LL::checkhighcard(Node *player[], Node *player2[])
 
 void LL::whowins(Node *player1[], Node *player2[])
 {
-  int playerval1 = 0;
-  int playerval2 = 0;
-
-
-
-  Node **playeroof1 = new Node*[10];
-  Node **playeroof2 = new Node*[10];
-
-  playeroof2[0] = new Node;
-  playeroof2[0]->value = 9;
-  playeroof2[0]->suit = 0;
-  playeroof2[1] = new Node;
-  playeroof2[1]->value = 8;
-  playeroof2[1]->suit = 1;
-  playeroof2[2] = new Node;
-  playeroof2[2]->value = 8;
-  playeroof2[2]->suit = 1;
-  playeroof2[3] = new Node;
-  playeroof2[3]->value = 7;
-  playeroof2[3]->suit = 1;
-  playeroof2[4] = new Node;
-  playeroof2[4]->value = 6;
-  playeroof2[4]->suit = 1;
-  playeroof2[5] = new Node;
-  playeroof2[5]->value = 3;
-  playeroof2[5]->suit = 0;
-  playeroof2[6] = new Node;
-  playeroof2[6]->value = 2;
-  playeroof2[6]->suit = 1;
-  playeroof2[7] = new Node;
-  playeroof2[7]->value = 0;
-  playeroof2[7]->suit = 0;
-  playeroof2[8] = new Node;
-  playeroof2[8]->value = 0;
-  playeroof2[8]->suit = 0;
-  playeroof2[9] = new Node;
-  playeroof2[9]->value = 0;
-  playeroof2[9]->suit = 0;
-
-
-
-
 
   pair(player1);
   pair(player2);
@@ -396,17 +330,17 @@ void LL::whowins(Node *player1[], Node *player2[])
   threeofakind(player1);
   threeofakind(player2);
 
-  straight(playeroof2);
-  straight(playeroof2);
+  straight(player1);
+  straight(player2);
 
-  flush(playeroof2);
-  flush(playeroof2);
+  flush(player1);
+  flush(player2);
 
-  fullhouse(playeroof2);
-  fullhouse(playeroof2);
+  fullhouse(player1);
+  fullhouse(player2);
 
-  //fourofakind(playeroof2);
-  //fourofakind(playeroof2);
+  fourofakind(player1);
+  fourofakind(player2);
 
   straightflush(player1);
   straightflush(player2);
@@ -671,65 +605,92 @@ void LL::whowins(Node *player1[], Node *player2[])
     }
     else if (player1[7]->value == 6)
     {
-
+      if (player1[7]->suit > player2[7]->suit)
+      {
+        player1[7]->value = 100;
+        cout<<"Player 1 wins"<<endl;
+      }
+      else if (player1[7]->suit < player2[7]->suit)
+      {
+        player2[7]->value = 100;
+        cout<<"Player 2 wins"<<endl;
+      }
+      else
+      {
+        if (player1[8]->value > player2[8]->value)
+        {
+          player1[7]->value = 100;
+          cout<<"Player 1 wins"<<endl;
+        }
+        else if (player1[8]->value < player2[8]->value)
+        {
+          player2[7]->value = 100;
+          cout<<"Player 2 wins"<<endl;
+        }
+        else
+        {
+          cout<<"Tie "<<endl;
+          player1[7]->value = 200;
+        }
+      }
     }
     else if (player1[7]->value == 7)
     {
-
+      if (player1[7]->suit > player2[7]->suit)
+      {
+        player1[7]->value = 100;
+        cout<<"Player 1 wins"<<endl;
+      }
+      else if (player1[7]->suit < player2[7]->suit)
+      {
+        player2[7]->value = 100;
+        cout<<"Player 2 wins"<<endl;
+      }
+      else
+      {
+        for (int i = 0; i < 5;i++)
+        {
+          if(player1[i]->value > player2[i]->value)
+          {
+            player1[7]->value = 100;
+            cout<<"Player 1 wins"<<endl;
+            break;
+          }
+          else if(player1[i]->value < player2[i]->value)
+          {
+            player2[7]->value = 100;
+            cout<<"Player 2 wins"<<endl;
+            break;
+          }
+          else
+          {
+            if (i == 5)
+            {
+              cout<<"Tie "<<endl;
+              player1[7]->value = 200;
+              break;
+            }
+          }
+        }
+      }
     }
     else if (player1[7]->value == 8)
     {
-
+      if (player1[7]->suit > player2[7]->suit)
+      {
+        player1[7]->value = 100;
+        cout<<"Player 1 wins"<<endl;
+      }
+      else if (player1[7]->suit < player2[7]->suit)
+      {
+        player2[7]->value = 100;
+        cout<<"Player 2 wins"<<endl;
+      }
+      else
+      {
+        cout<<"Tie "<<endl;
+        player1[7]->value = 200;
+      }
     }
   }
 }
-
-/*
-void LL::straight(Node *player[])
-{
-  int k=0;
-  int m=0;
-  for(int i=0; i<3; i++){
-    int j=0;
-    m = i;
-    while(player[i+1]->value == (player[i]->value-1) || player[i+1]->value == player[i]->value){
-      if(player[i+1]->value != player[i]->value){
-        j++;
-      }
-      i++;
-      if(j == 4){
-        player[7]->value = 4;
-        k=1;
-        break;
-      }
-    }
-    if(k == 1){
-      player[7]->suit = player[m]->value;
-      break;
-    }
-    i=m;
-  }
-  if((player[7]->value != 4) && (player[1]->value == 14)){
-    int x = player[7]->value;
-    player[7]->value = 1;
-    int p = 4;
-    int n = 0;
-    while(player[p+1]->value == (player[p]->value-1) || player[p+1]->value == player[p]->value){
-      if(player[p+1]->value != player[p]->value){
-        n++;
-      }
-      p++;
-      if(n == 4){
-        player[7]->value = 4;
-        k=1;
-        break;
-      }
-    }
-    if(k == 1){
-      player[7]->suit = player[3]->value;
-    }
-  }
-  cout<<"???????????????????????"<<endl;
-  cout<<player[7]->value<<endl;
-  cout<<player[7]->suit<<endl<<endl;
-}*/
